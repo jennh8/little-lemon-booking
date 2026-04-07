1c4fcfdab163a22a2f7f617b392a18564c531458
jennahongisto <n2hoje00@students.oamk.fi>
2026-04-07 16:12:55 +0300

    style specials

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
 import CallToAction from "./CallToAction";
+import Specials from "./Specials";
 
 export default function Main() {
 return(
     <>
         <CallToAction/>
+        <Specials/>
     </>
 )
 }
--- src/Specials.js
+++ src/Specials.js
@@ -1,7 +1,57 @@
+import Lemon from './assets/lemon-dessert.jpg'
+import Bruschetta from './assets/bruchetta.svg'
+import Greek from './assets/greek-salad.jpg'
+
 export default function Specials() {
 return(
-    <div>
-        
+    <div className="spec">
+        <div className="spec-heading">
+            <h1>This week's specials!</h1>
+            <button>
+                <span>Online Menu</span>
+            </button>
+        </div>
+        <div className="cards">
+            <article className="article">
+                <div className='card-img'>
+                    <img src={Greek}></img>
+                </div>
+                <div className='card-content'>
+                    <div className='dish'>
+                        <h4>Greek Salad</h4>
+                        <h4>$12.99</h4>
+                    </div>
+                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                </div>
+                    <button>Order a delivery</button>
+            </article>
+            <article className="article">
+                <div className='card-img'>
+                    <img src={Bruschetta}></img>
+                </div>
+                <div className='card-content'>
+                    <div className='dish'>
+                        <h4>Bruschetta</h4>
+                        <h4>$5.99</h4>
+                    </div>
+                    <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
+                </div>
+                    <button>Order a delivery</button>
+            </article>
+            <article className="article">
+                <div className='card-img'>
+                    <img src={Lemon}></img>
+                </div>
+                <div className='card-content'>
+                    <div className='dish'>
+                        <h4>Lemon Dessert</h4>
+                        <h4>$5.00</h4>
+                    </div>
+                    <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                </div>
+                    <button>Order a delivery</button>
+            </article>
+        </div>
     </div>
 )
 }
