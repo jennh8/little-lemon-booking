b07e442089c29606d74e2d03e644b4f910e3c564
jennahongisto <n2hoje00@students.oamk.fi>
2026-04-11 12:03:36 +0300

    style about

--- src/Chicago.js
+++ src/Chicago.js
@@ -1,7 +1,19 @@
+import About from './assets/Mario-and-Adrian.jpg'
+
 export default function Chicago() {
 return(
-    <div>
-        
+    <div className="chicago">
+        <div>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <span>Little Lemon is a family-owned Mediterranean restaurant blending
+                tradition with a modern twist. We serve fresh, flavorful dishes inspired
+                by classic recipes, reimagined with contemporary techniques. Every meal
+                is made with care—welcoming you like part of the family.</span>
+        </div>
+        <div>
+            <img src={About}></img>
+        </div>
     </div>
 )
 }
