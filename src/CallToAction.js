298a30cd7146b50ea73cfa39657da050b9591b54
jennahongisto <n2hoje00@students.oamk.fi>
2026-04-11 09:42:33 +0300

    add fonts

--- src/CallToAction.js
+++ src/CallToAction.js
@@ -9,7 +9,7 @@ return(
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
                 <span className='cta-span'>We are a family owned Mediterranean restaurant, focused on recipes served with a modern twist</span>
-                <button className='cta-btn'>
+                <button>
                     <span>Reserve a Table</span>
                 </button>
             </div>
