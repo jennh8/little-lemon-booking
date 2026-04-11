4d409df11ebc2b59d88a4761a4155b4941399390
jennahongisto <n2hoje00@students.oamk.fi>
2026-04-11 11:49:52 +0300

    style testimonials

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,15 @@
 import CallToAction from "./CallToAction";
+import Chicago from "./Chicago";
 import Specials from "./Specials";
+import Testimonials from "./Testimonials";
 
 export default function Main() {
 return(
     <>
         <CallToAction/>
         <Specials/>
+        <Testimonials/>
+        <Chicago/>
     </>
 )
 }
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,7 +1,44 @@
+import User from './assets/user.png'
+
 export default function Testimonials() {
 return(
-    <div>
-        
+    <div className="testimonials">
+        <h1>Testimonials</h1>
+        <div className="test-cards">
+            <article>
+                <h3>Rating</h3>
+                <div className='user'>
+                    <img src={User}></img>
+                    <h3>Jen</h3>
+                    
+                </div>
+                <button>Review text</button>
+            </article>
+            <article>
+                <h3>Rating</h3>
+                <div className='user'>
+                    <img src={User}></img>
+                    <h3>Tommy</h3>
+                </div>
+                <button>Review text</button>
+            </article>
+            <article>
+                <h3>Rating</h3>
+                <div className='user'>
+                    <img src={User}></img>
+                    <h3>Evelyn</h3>
+                </div>
+                <button>Review text</button>
+            </article>
+            <article>
+                <h3>Rating</h3>
+                <div className='user'>
+                    <img src={User}></img>
+                    <h3>Liam</h3>
+                </div>
+                <button>Review text</button>
+            </article>
+        </div>
     </div>
 )
 }
