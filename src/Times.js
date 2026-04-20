ddd12a9e1d5efb48931b0202fab9169b59429652
jennahongisto <n2hoje00@students.oamk.fi>
2026-04-20 17:22:12 +0300

    add api

--- src/Times.js
+++ src/Times.js
@@ -1,11 +1,13 @@
+import { fetchAPI } from "./api";
+
 export function initializeTimes() {
-  return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  return fetchAPI(new Date());
 }
 
 export function updateTimes(state, action) {
   switch (action.type) {
     case "UPDATE_TIMES":
-      return initializeTimes();
+      return fetchAPI(new Date(action.date));
     default:
       return state;
   }
